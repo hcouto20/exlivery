7745db38de0897b565352db6b111b875047a8987
Helder Couto <helder@brassun.com.br>
2022-01-20 17:33:34 -0300

    Teste order, factory order

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,7 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Item, Order}
   alias Exlivery.Users.User
 
   def user_factory do
@@ -20,6 +20,23 @@ defmodule Exlivery.Factory do
       category: :pizza,
       unity_price: Decimal.new("48.90"),
       quantity: 1
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "Rua das bananas, 749",
+      items: [
+        build(:item),
+        build(:item,
+          description: "Temaki de salmão",
+          category: :japonesa,
+          quantity: 2,
+          unity_price: Decimal.new("17.5")
+        )
+      ],
+      total_price: Decimal.new("83.90"),
+      user_cpf: "12345678900"
     }
   end
 end
