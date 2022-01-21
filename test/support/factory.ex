c7266464b7865017105fe35b997ef6ad849813a6
Helder Couto <helder@brassun.com.br>
2022-01-21 18:26:24 -0300

    Alterações no factory, implementado AgentTest, alterado user_test ordem atributos

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -6,11 +6,11 @@ defmodule Exlivery.Factory do
 
   def user_factory do
     %User{
-      name: "Helder",
-      email: "helder@banana.com",
+      address: "Rua das bananas, 749",
+      name: "Pedro",
+      email: "pedro@banana.com",
       cpf: "12345678900",
-      age: 38,
-      address: "Rua das bananas, 749"
+      age: 38
     }
   end
 
